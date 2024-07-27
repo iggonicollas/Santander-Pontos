8f7e8301f3c99e2e405b20fa9c536c1143177811
iggonicollas <iggonicollas@gmail.com>
2024-07-27 10:41:47 -0300

    Adcionando arquivos iniciais

---
+++ src/main/java/com/santanderpoints/SantanderPontosApplication.java
@@ -0,0 +1,13 @@
+package com.santanderpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderPontosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderPontosApplication.class, args);
+	}
+
+}
